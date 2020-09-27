d74f9b4cc93f061b4a378263e8f56d6c12274490
yanglin <494657028@qq.com>
2020-09-27 23:38:15 +0800

    add BFS与DFSDFS详解

--- .vuepress/config/themeConf.js
+++ .vuepress/config/themeConf.js
@@ -3,7 +3,7 @@ const sidebar = require('./sidebar')
 module.exports = {
   // 要部署的仓库名字
   base: '/',
-  repo: 'Yutiy/blog',
+  repo: 'Yutiy/algorithm',
   navbar: true,
   editLinks: true,
   editLinkText: "在 GitLab 上编辑此页",
@@ -16,8 +16,8 @@ module.exports = {
   nav: [
     { text: 'leetcode', link: '/leetcode/' },
     { text: '设计模式', link: '/design_pattern/' },
-    { text: '排序算法', link: '/bubble_sort/' },
-    { text: '数据结构', link: '/queue/' },
+    { text: '排序算法', link: '/sort/bubble/' },
+    { text: '数据结构', link: '/data-structure/queue/' },
     { text: 'thinking', link: '/thinking/complex/' },
   ]
 }
