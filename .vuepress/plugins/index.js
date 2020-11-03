bda80f2c50548f2d25070932a6f7ada9fbbef363
yanglin <494657028@qq.com>
2020-11-03 11:00:29 +0800

    添加题目序号

--- .vuepress/plugins/index.js
+++ .vuepress/plugins/index.js
@@ -17,7 +17,6 @@ module.exports = [
     }
   ],
   '@vuepress-reco/vuepress-plugin-back-to-top',
-  ['@vuepress/google-analytics', { ga: 'UA-124601890-2' }],
   '@vuepress/medium-zoom',
   [
     '@vuepress/last-updated',
@@ -37,21 +36,22 @@ module.exports = [
       }
     }
   ],
-  [
-    'vuepress-plugin-comment',
-    {
-      clientID: '3cc86f34770ab7a66bb1',
-      clientSecret: process.env.clientSecret || '',
-      repo: 'Blog',
-      owner: 'Yutiy',
-      admin: ['Yutiy'],
-      id: '<%- frontmatter.commentid || frontmatter.permalink %>', // Ensure uniqueness and length less than 50
-      distractionFreeMode: false, // Facebook-like distraction free mode
-      labels: ['Gitalk', 'Comment'],
-      title: '「评论」<%- frontmatter.title %>',
-      body:
-        '<%- frontmatter.title %>：<%- window.location.origin %><%- frontmatter.to.path || window.location.pathname %>'
-    }
-  ],
   ['vuepress-plugin-code-copy', true]
+  // ['@vuepress/google-analytics', { ga: 'UA-124601890-2' }],
+  // [
+  //   'vuepress-plugin-comment',
+  //   {
+  //     clientID: '3cc86f34770ab7a66bb1',
+  //     clientSecret: process.env.clientSecret || '',
+  //     repo: 'Blog',
+  //     owner: 'Yutiy',
+  //     admin: ['Yutiy'],
+  //     id: '<%- frontmatter.commentid || frontmatter.permalink %>', // Ensure uniqueness and length less than 50
+  //     distractionFreeMode: false, // Facebook-like distraction free mode
+  //     labels: ['Gitalk', 'Comment'],
+  //     title: '「评论」<%- frontmatter.title %>',
+  //     body:
+  //       '<%- frontmatter.title %>：<%- window.location.origin %><%- frontmatter.to.path || window.location.pathname %>'
+  //   }
+  // ],
 ]
